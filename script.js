f84bb5201924b8842ea5737b40fc070fda885ce9
TomKrok1 <91782249+trak2022z@users.noreply.github.com>
2022-03-23 17:58:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+(function() {
+window.addEventListener("load", init);
+
+  function init() {
+
+    console.log('page loaded');
+    
+    let demobtn1 = document.getElementById("demo-btn1");
+    demobtn1.addEventListener("click", clickHandler);
+
+    disableMenu();
+}
+
+  
+function disableMenu() {
+  let outputtext2 = document.getElementById("output-text2");
+  outputtext2.classList.add('hidden');
+}
+
+function clickHandler() {
+  let outputtext1 = document.getElementById("output-text1");
+  outputtext1.textContent = "Wait for it...";
+  setTimeout(sayHello1, 5000);
+}
+
+function sayHello1() { // called when the timer goes off
+  let outputtext1 = document.getElementById("output-text1");
+  outputtext1.textContent = "Hello1!";
+}
+
+})();
